9d0ae6e8f93224cb55436e029c2a370edbe45c00
dernasherbrezon <rodionovamp@mail.ru>
2019-03-05 23:15:50 +0000

    initial version

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
+tar xzf bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
+./jdk-11.0.2/bin/jlink --add-modules java.base,java.desktop,java.logging --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
+mvn clean deploy

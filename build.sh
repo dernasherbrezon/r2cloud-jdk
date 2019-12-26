232932ab24a67f52112e8c62e092ff0d8fd84f12
dernasherbrezon <dernasherbrezon@gmail.com>
2019-12-26 15:46:46 +0000

    upgrade jdk

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/bell-sw/Liberica/releases/download/11.0.2/bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
-tar xzf bellsoft-jdk11.0.2-linux-arm32-vfp-hflt.tar.gz
-./jdk-11.0.2/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
-mvn clean deploy
+wget https://github.com/bell-sw/Liberica/releases/download/11.0.5/bellsoft-jdk11.0.5-linux-arm32-vfp-hflt.tar.gz
+tar xzf bellsoft-jdk11.0.5-linux-arm32-vfp-hflt.tar.gz
+./jdk-11.0.5/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http,jdk.crypto.cryptoki --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
+#mvn clean deploy

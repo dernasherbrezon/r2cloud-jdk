4fd7d7c8333798afb7cae2faa129a63bb5f688f7
dernasherbrezon <rodionovamp@mail.ru>
2020-11-29 11:05:32 +0000

    upgrade to 11.0.9

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/bell-sw/Liberica/releases/download/11.0.5/bellsoft-jdk11.0.5-linux-arm32-vfp-hflt.tar.gz
-tar xzf bellsoft-jdk11.0.5-linux-arm32-vfp-hflt.tar.gz
-./jdk-11.0.5/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http,jdk.crypto.cryptoki --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
+wget https://github.com/bell-sw/Liberica/releases/download/11.0.9.1%2B1/bellsoft-jdk11.0.9.1+1-linux-arm32-vfp-hflt.tar.gz
+tar xzf bellsoft-jdk11.0.9.1+1-linux-arm32-vfp-hflt.tar.gz
+./jdk-11.0.9.1/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http,jdk.crypto.cryptoki --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
 #mvn clean deploy

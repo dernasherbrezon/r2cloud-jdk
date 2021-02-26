9dcc565a011df0882ed904f7214700543091735b
dernasherbrezon <rodionovamp@mail.ru>
2021-02-26 10:26:38 +0000

    fix jdk version and perform minor upgrade

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/bell-sw/Liberica/releases/download/11.0.9.1%2B1/bellsoft-jdk11.0.9.1+1-linux-arm32-vfp-hflt.tar.gz
-tar xzf bellsoft-jdk11.0.9.1+1-linux-arm32-vfp-hflt.tar.gz
-./jdk-11.0.9.1/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http,jdk.crypto.cryptoki --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
+wget https://download.bell-sw.com/java/11.0.10+9/bellsoft-jdk11.0.10+9-linux-arm32-vfp-hflt.tar.gz
+tar xzf bellsoft-jdk11.0.10+9-linux-arm32-vfp-hflt.tar.gz
+./jdk-11.0.10/bin/jlink --add-modules java.base,java.desktop,java.logging,java.net.http,jdk.crypto.cryptoki --compress=2 --no-header-files --no-man-pages --output ./src/main/deb/jdk
 #mvn clean deploy
